ee2706708712e7ca9e621da06bf1436c35a21a95
om kumar singh <omsingh.ethdev@gmail.com>
2026-08-11 12:30:18 +0530

    feat: add logging to disk module functions

--- engine/disk.sh
+++ engine/disk.sh
@@ -1,8 +1,17 @@
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+source ${SCRIPT_DIR}/../lib/log.sh
+
+
 #Description:List physical disk devices, excluding loop and rom devices
 #Argument:NONE
 #Returns: Populates disks_name array with disk name,size,type
 disk_list() {
     mapfile -t disk_names < <(lsblk -d -n -o NAME,SIZE,TYPE| grep -w disk)
+
+    if [ ${#disk_names[@]} -eq 0 ]; then
+        log "WARN" "disk" "No physical disk found" 
+        return 1
+    fi
 }
 
 #Description:List a disk's partitions size, filesystem type and mount status 
@@ -11,6 +20,15 @@ disk_list() {
 #Returns: Populates disk_details array with disk/partition info 
 disk_info(){
     local disk_name="$1"
+    if [ -z "${disk_name}" ]; then
+        log "WARN" "disk" "No disk name provided to disk_info"
+        return 1
+    fi
+
+    if ! disk_exists "${disk_name}"; then
+        log "WARN" "disk" "Given disk named ${disk_name} does'nt exist"
+        return 1
+    fi
     mapfile -t disk_details < <(lsblk -n -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINT /dev/$disk_name)
 }
 
@@ -24,9 +42,12 @@ disk_exists(){
     if [ -b "/dev/$disk_name" ]; then
         return 0
     else
+        log "WARN" "disk" "Disk $disk_name does not exitst" 
         return 1
     fi
 }
+
+
 
 # Description: Gives the type of disk -SSD or -HDD
 # Arguments:
@@ -37,6 +58,21 @@ disk_exists(){
 
 disk_type() {
     local disk_name="$1"
+    if [ -z "${disk_name}" ]; then
+        log "WARN" "disk" "No disk name provided to disk_type"
+        return 1
+    fi
+
+    if ! disk_exists "${disk_name}"; then
+        log "WARN" "disk" "Given disk named ${disk_name} does'nt exist"
+        return 1
+    fi
+
+    if [ ! -f "/sys/block/${disk_name}/queue/rotational" ]; then
+        log "WARN" "disk" "Rotational information not available for disk ${disk_name}"
+        return 1
+    fi
+
     local rotational=$(cat /sys/block/$disk_name/queue/rotational)
 
     if [ "${rotational}" -eq 0 ]; then
@@ -45,4 +81,4 @@ disk_type() {
         echo "HDD"
     fi
 }
-disk_type sdb
+
--- lib/log.sh
+++ lib/log.sh
@@ -1,3 +1,9 @@
+#Description: Appends a timestamped log entry (level, module, message) to log/diskpilot.log 
+#Arguments:
+#   $1 = info/error/warn
+#   $2 = module name
+#   $3 = message
+#Returns: Appends a timestamped log entry to logs/diskpilot.log
 log() {
     time_stamp=$(date "+%Y-%m-%d %H:%M:%S")
     local SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
